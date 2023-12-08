e6e373170e7865bd68b4bb9838a52a08883aed1a
kotaroito <agkito34@gmail.com>
2023-12-08 13:08:00 -0800

    update app from SPA to MPA

---
+++ frontend/src/components/index.js
@@ -0,0 +1,6 @@
+import Filter from "./Filter";
+import FilterList from "./FilterList";
+import TableItem from "./TableItem";
+import SearchBar from "./SearchBar";
+
+export { Filter, FilterList, TableItem, SearchBar };
